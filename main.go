07f24ce833eda207092e53f5cee3bcd7d4af7cbd
Nokodoko <cMonty614@gmail.com>
2020-07-09 15:57:07 -0400

    Added stucts, began forming methods

--- main.go
+++ main.go
@@ -15,20 +15,50 @@ func main() {
 //2. Make actions (methods)
 //3. Create scenarios (switches)
 
-type name struct{
-    field type
+type FinishMoves struct {
+	Painless string
+	Painful  string
+	Brutal   string
 }
 
 type FightType struct {
-	Movement int  //Attack speed (scale of 1 to 10)
-	Weapon   bool //Does this charcter use a weapon: if yes, add kill method will weapon
-	Strength int  //Take more LifePoints points per hit
-	Finish   FinishMoves
+	Movement   int  //Attack speed (scale of 1 to 10)
+	Weapon     bool //Does this charcter use a weapon: if yes, add kill method will weapon
+	Strength   int  //Take more LifePoints points per hit
+	ComboChain string
+	Finish     FinishMoves
 }
 
 type Person struct {
+	name         string
+	relationship Fighter
+	LifePoints   int
+}
+
+func (p Person) loseLife() int {
+	return 0
+}
+
+func (p Person) attack(p2 Fighter) int {
+	return 0
+}
+
+type Fighter struct {
 	Name          string
 	Aliases       []string
 	LifePoints    int
 	FightingStyle fightType
+}
+
+func (p Fighter) attack(p2 Fighter) int {
+	return 0
+}
+
+func (p Fighter) loseLife() int {
+	return 0
+}
+
+type Player interface {
+	attack()
+	loseLife()
 }
